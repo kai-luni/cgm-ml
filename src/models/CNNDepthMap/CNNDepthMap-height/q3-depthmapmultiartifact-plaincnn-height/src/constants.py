18819fe156772f57cd33ffd62e7e3bd591795032
Markus Hinsche <m.hinsche@gmail.com>
2021-01-20 15:24:26 +0100

    Put pip packages to constants.py instead of notebook (#242)
    Motivation: It is easy to miss to update pip requirements in notebooks. When using modules, it is easier to see changes in git and to find all occurrences in the code.

--- src/models/CNNDepthMap/CNNDepthMap-height/q3-depthmapmultiartifact-plaincnn-height/src/constants.py
+++ src/models/CNNDepthMap/CNNDepthMap-height/q3-depthmapmultiartifact-plaincnn-height/src/constants.py
@@ -1,3 +1,13 @@
 from pathlib import Path
 
 REPO_DIR = Path(__file__).parents[6].absolute()
+
+PIP_PACKAGES = [
+    "azureml-dataprep[fuse,pandas]",
+    "glob2",
+    "opencv-python==4.1.2.30",
+    "matplotlib",
+    "imgaug==0.4.0",
+    "tensorflow-addons==0.11.2",
+    "bunch==1.0.1",
+]
--- src/models/Unsupervised/Autoencoder/q4-anomalydetection-ae/src/constants.py
+++ src/models/Unsupervised/Autoencoder/q4-anomalydetection-ae/src/constants.py
@@ -3,3 +3,13 @@ from pathlib import Path
 REPO_DIR = Path(__file__).parents[6].absolute()
 DATA_DIR_ONLINE_RUN = Path("/tmp/data/")
 MODEL_CKPT_FILENAME = "best_model.ckpt"
+
+PIP_PACKAGES = [
+    "azureml-dataprep[fuse,pandas]",
+    "glob2",
+    "tensorflow_datasets",
+    "opencv-python==4.1.2.30",
+    "matplotlib",
+    "imageio",
+    "sklearn"
+]
