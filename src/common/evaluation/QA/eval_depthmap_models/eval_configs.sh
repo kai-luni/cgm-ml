c8e7f4837ec3eaa76f2306bb1ebbb0c00dc78520
Markus Hinsche <m.hinsche@gmail.com>
2021-01-22 17:53:35 +0100

    Rename configs in model evaluation pipeline (#249)
    Changes:
    - Rename configs so they are more intuitive to understand
    - Enable eval azure pipeline for special branches

--- src/common/evaluation/QA/eval_depthmap_models/eval_configs.sh
+++ src/common/evaluation/QA/eval_depthmap_models/eval_configs.sh
@@ -2,8 +2,8 @@
 
 set -euox pipefail
 
-python eval_main.py --qa_config_module qa_config_42c4ef33
-python eval_main.py --qa_config_module qa_config_cb44f6db
+python eval_main.py --qa_config_module qa_config_height_no_dropout
+python eval_main.py --qa_config_module qa_config_height_dropout
 # python eval_main.py --qa_config_module qa_config_filter
 # python eval_main.py --qa_config_module qa_config_height
 # python eval_main.py --qa_config_module qa_config_weight
