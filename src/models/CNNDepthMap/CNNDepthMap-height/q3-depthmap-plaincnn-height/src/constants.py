58ca053b3e1766387bfc4a3c80b64b5e2e4f02df
Markus Hinsche <m.hinsche@gmail.com>
2021-02-08 09:21:10 +0100

    Train with py37 (#270)
    **Motivation:** 
    - For some [new features](https://github.com/Welthungerhilfe/cgm-ml/pull/268), we would need python 3.7 but AzureML supports python 3.6
    - Also we get the warning `WARNING - 'TensorFlow' estimator is deprecated. Please use 'ScriptRunConfig' from 'azureml.core.script_run_config' with your own defined environment or one of the Azure ML TensorFlow curated environments.`(https://docs.microsoft.com/en-us/azure/machine-learning/how-to-migrate-from-estimators-to-scriptrunconfig)
    
    **Solution:** we have to upgrade to use `ScriptRunConfig` and build our own Environment.
    
    **Changes:**
    - Change from `Tensorflow` estimator to `ScriptRunConfig`
        - attach dataset argument in case of mounting 
    - build own environment
        - `Environment.from_conda_specification()` works because we can specify the python version (Unfortunately I didn't find a way to specify the version when using `Environment.from_pip_requirements()`)
        - built and persisted our environment (to avoid time-wasting re-build for every Run)

--- src/models/CNNDepthMap/CNNDepthMap-height/q3-depthmap-plaincnn-height/src/constants.py
+++ src/models/CNNDepthMap/CNNDepthMap-height/q3-depthmap-plaincnn-height/src/constants.py
@@ -3,13 +3,3 @@ from pathlib import Path
 REPO_DIR = Path(__file__).parents[6].absolute()
 DATA_DIR_ONLINE_RUN = Path("/tmp/data/")
 MODEL_CKPT_FILENAME = "best_model.ckpt"
-
-PIP_PACKAGES = [
-    "azureml-dataprep[fuse,pandas]",
-    "glob2",
-    "opencv-python==4.1.2.30",
-    "matplotlib",
-    "tensorflow-addons==0.11.2",
-    "bunch==1.0.1",
-    "wandb",
-]
