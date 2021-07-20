73693f1c4a196a75035616cf61373c0f4b96e070
Markus Hinsche <m.hinsche@gmail.com>
2021-07-20 19:24:37 +0200

    Train RGBD model (#308)
    **Changes:** 
    - adjust shape (4 channels instead of 1), adjust tf_load_pickle
    - normalize depthmap by 7.5, normalizing RGB channels by dividing by the maximum pixel value which is 255
    - use LR find to find good initial learning rate

---
+++ src/models/CNNDepthMap/CNNDepthMap-height/2021q3-depthmap-plaincnn-rgbd/src/config.py
@@ -0,0 +1,27 @@
+from bunch import Bunch
+
+DATASET_MODE_DOWNLOAD = "dataset_mode_download"
+DATASET_MODE_MOUNT = "dataset_mode_mount"
+
+CONFIG = Bunch(dict(
+    DATASET_MODE=DATASET_MODE_DOWNLOAD,
+    DATASET_NAME="anon-rgbd-5kscans",
+    DATASET_NAME_LOCAL="anon-rgbd-5kscans-mini",  # 20 qrcodes
+    SPLIT_SEED=0,
+    IMAGE_TARGET_HEIGHT=240,
+    IMAGE_TARGET_WIDTH=180,
+    EPOCHS=1000,
+    BATCH_SIZE=64,
+    SHUFFLE_BUFFER_SIZE=640,
+    NORMALIZATION_VALUE=7.5,
+    LEARNING_RATE=0.0003,
+    USE_ONE_CYCLE=True,
+    USE_DROPOUT=False,
+    USE_WANDB=False,
+    USE_MULTIGPU=False,
+    CLUSTER_NAME='gpu-cluster-M60',
+
+    # Parameters for dataset generation.
+    TARGET_INDEXES=[0],  # 0 is height, 1 is weight.
+    CODES=['100', '101', '102', '200', '201', '202'],
+))
---
+++ src/models/CNNDepthMap/CNNDepthMap-height/2021q3-depthmap-plaincnn-rgbd/src/constants.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+REPO_DIR = Path(__file__).parents[6].absolute()
+DATA_DIR_ONLINE_RUN = Path("/tmp/data/")
+MODEL_CKPT_FILENAME = "best_model.ckpt"
+NUM_INPUT_CHANNELS = 4
