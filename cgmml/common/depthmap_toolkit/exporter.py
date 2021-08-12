7cff819c684354519ff0520d47d6680786c39760
Luboš Vonásek <tridosm@gmail.com>
2021-08-12 14:02:35 +0200

    Add method to export renderable pointcloud (#437)
    * Add method to export renderable pointcloud
    
    * Make flake8 happy
    
    Co-authored-by: Lubos Vonasek <Lubos.Vonasek@welthungerhilfe.de>

--- cgmml/common/depthmap_toolkit/exporter.py
+++ cgmml/common/depthmap_toolkit/exporter.py
@@ -46,19 +46,57 @@ def export_obj(fpath: Union[str, Path],
                 depth = dmap.depthmap_arr[x, y]
                 if not depth:
                     continue
-                #if not points_3d_arr[x, y]:
-                    #continue
                 count = count + 1
                 indices[x, y] = count  # add index of written vertex into array
 
                 x_coord = points_3d_arr[0, x, y]
-                y_coord = points_3d_arr[1, x, y]
-                depth_coord = points_3d_arr[2, x, y]
-                f.write(f'v {x_coord} {y_coord - floor_altitude_in_meters} {depth_coord}\n')
+                y_coord = points_3d_arr[1, x, y] - floor_altitude_in_meters
+                z_coord = points_3d_arr[2, x, y]
+                f.write(f'v {x_coord} {y_coord} {z_coord}\n')
                 f.write(f'vt {x / dmap.width} {1 - y / dmap.height}\n')
 
         if triangulate:
             _do_triangulation(dmap, indices, f)
+        logger.info('Mesh exported into %s', fpath)
+
+
+def export_renderable_obj(fpath: Union[str, Path],
+                          dmap: Depthmap,
+                          floor_altitude_in_meters: float,
+                          point_size_in_meters: float):
+    """Export pointcloud as .obj file, which can be rendered in tools like Blender.
+
+    floor_altitude_in_meters is the floor altitude to align floor to Y=zero
+    point_size_in_meters is point size in meters
+    """
+    fpath = Path(fpath)
+    count = 1
+
+    # Create MTL file (a standart extension of OBJ files to define geometry materials and textures)
+    material_fpath = fpath.with_suffix('.mtl')
+    if dmap.has_rgb:
+        with open(material_fpath, 'w') as f:
+            f.write('newmtl default\n')
+            f.write(f'map_Kd {str(dmap.rgb_fpath.absolute())}\n')
+
+    with open(fpath, 'w') as f:
+        if dmap.has_rgb:
+            f.write(f'mtllib {material_fpath.name}\n')
+            f.write('usemtl default\n')
+
+        points_3d_arr = dmap.convert_2d_to_3d_oriented()
+        for x in range(2, dmap.width - 2):
+            for y in range(2, dmap.height - 2):
+                depth = dmap.depthmap_arr[x, y]
+                if not depth:
+                    continue
+
+                x_coord = points_3d_arr[0, x, y]
+                y_coord = points_3d_arr[1, x, y] - floor_altitude_in_meters
+                z_coord = points_3d_arr[2, x, y]
+                _write_obj_cube(f, dmap, x, y, count, x_coord, y_coord, z_coord, point_size_in_meters)
+                count = count + 8
+
         logger.info('Mesh exported into %s', fpath)
 
 
@@ -81,7 +119,7 @@ def _do_triangulation(dmap: Depthmap, indices, filehandle):
                     b = str(int(indices[x + 1, y]))
                     a = str(int(indices[x, y + 1]))
                     # define triangle indices in (world coordinates / texture coordinates)
-                    filehandle.write(f'f {a}/{a} {b}/{b} {c}/{c}\n')
+                    _write_obj_triangle_indices(filehandle, a, b, c)
 
             # check if second triangle points have existing indices
             if indices[x + 1, y + 1] > 0 and indices[x + 1, y] > 0 and indices[x, y + 1] > 0:
@@ -92,7 +130,65 @@ def _do_triangulation(dmap: Depthmap, indices, filehandle):
                     b = str(int(indices[x + 1, y]))
                     c = str(int(indices[x, y + 1]))
                     # define triangle indices in (world coordinates / texture coordinates)
-                    filehandle.write(f'f {a}/{a} {b}/{b} {c}/{c}\n')
+                    _write_obj_triangle_indices(filehandle, a, b, c)
+
+
+def _write_obj_cube(
+        f,
+        dmap: Depthmap,
+        x: int,
+        y: int,
+        count: int,
+        x_coord: float,
+        y_coord: float,
+        z_coord: float,
+        size: float):
+
+    # cube points
+    f.write(f'v {x_coord - size} {y_coord - size} {z_coord - size}\n')
+    f.write(f'vt {x / dmap.width} {1 - y / dmap.height}\n')  # 0 ---
+    f.write(f'v {x_coord + size} {y_coord - size} {z_coord - size}\n')
+    f.write(f'vt {x / dmap.width} {1 - y / dmap.height}\n')  # 1 +--
+    f.write(f'v {x_coord - size} {y_coord + size} {z_coord - size}\n')
+    f.write(f'vt {x / dmap.width} {1 - y / dmap.height}\n')  # 2 -+-
+    f.write(f'v {x_coord + size} {y_coord + size} {z_coord - size}\n')
+    f.write(f'vt {x / dmap.width} {1 - y / dmap.height}\n')  # 3 ++-
+    f.write(f'v {x_coord - size} {y_coord - size} {z_coord + size}\n')
+    f.write(f'vt {x / dmap.width} {1 - y / dmap.height}\n')  # 4 --+
+    f.write(f'v {x_coord + size} {y_coord - size} {z_coord + size}\n')
+    f.write(f'vt {x / dmap.width} {1 - y / dmap.height}\n')  # 5 +-+
+    f.write(f'v {x_coord - size} {y_coord + size} {z_coord + size}\n')
+    f.write(f'vt {x / dmap.width} {1 - y / dmap.height}\n')  # 6 -++
+    f.write(f'v {x_coord + size} {y_coord + size} {z_coord + size}\n')
+    f.write(f'vt {x / dmap.width} {1 - y / dmap.height}\n')  # 7 +++
+
+    # front face
+    _write_obj_triangle_indices(f, str(count + 2), str(count + 1), str(count + 0))
+    _write_obj_triangle_indices(f, str(count + 3), str(count + 2), str(count + 1))
+
+    # back face
+    _write_obj_triangle_indices(f, str(count + 4), str(count + 5), str(count + 6))
+    _write_obj_triangle_indices(f, str(count + 7), str(count + 6), str(count + 5))
+
+    # left face
+    _write_obj_triangle_indices(f, str(count + 2), str(count + 4), str(count + 0))
+    _write_obj_triangle_indices(f, str(count + 2), str(count + 4), str(count + 6))
+
+    # right face
+    _write_obj_triangle_indices(f, str(count + 1), str(count + 5), str(count + 3))
+    _write_obj_triangle_indices(f, str(count + 7), str(count + 5), str(count + 3))
+
+    # top face
+    _write_obj_triangle_indices(f, str(count + 6), str(count + 3), str(count + 2))
+    _write_obj_triangle_indices(f, str(count + 3), str(count + 6), str(count + 7))
+
+    # bottom face
+    _write_obj_triangle_indices(f, str(count + 0), str(count + 1), str(count + 4))
+    _write_obj_triangle_indices(f, str(count + 4), str(count + 3), str(count + 1))
+
+
+def _write_obj_triangle_indices(f, a: str, b: str, c: str):
+    f.write(f'f {a}/{a} {b}/{b} {c}/{c}\n')
 
 
 def _write_pcd_header(filehandle, count):
