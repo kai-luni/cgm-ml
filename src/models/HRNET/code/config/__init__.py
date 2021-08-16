a53b280db15514dc906022c1bc034f03dadddd0f
Prajwal kumar singh <prajwalsingh651@gmail.com>
2021-08-16 12:13:44 +0530

    Revert "Analyse and result generation code of pose prediction model (#322)" (#445)
    This reverts commit cdbd48c1beb7a69ec0d2f28b42ba4f4f779d84ac.

--- src/models/HRNET/code/config/__init__.py
+++
@@ -1,3 +0,0 @@
-from models.HRNET.code.config.default import _C as cfg  # noqa
-from models.HRNET.code.config.default import update_config  # noqa
-from models.HRNET.code.config.models import MODEL_EXTRAS  # noqa
