037f040dc910973ac139ddb681a6fa61cddc53e9
Prajwal kumar singh <prajwalsingh651@gmail.com>
2021-08-16 18:32:28 +0530

    Pose hrnet (#446)
    * imports added
    
    * keypoints and cateogry added
    
    * transformation added
    
    * importing library
    
    * prepare_output_dirs updated
    
    * cuda torch available
    
    * importing box model
    
    * read image
    
    * experiment updated
    
    * remove some code
    
    * remove .git
    
    * lib file added
    
    * remove nms
    
    * demo code added
    
    * remove code duplicacy
    
    * remove drawing functioins
    
    * reame added
    
    * line saved!!
    
    * remove some default
    
    * line saved!!
    
    * poseeval removed
    
    * remove some code
    
    * remove imports
    
    * updated code
    
    * remove code
    
    * code added
    
    * rename value
    
    * updated code
    
    * lines removed
    
    * lines removed
    
    * output removed
    
    * files updated
    
    * flake8 happy
    
    * config files added
    
    * remove commented line
    
    * Added place holder for posenet evaluation
    
    * Added code for auth
    
    * Added code for starting the script
    
    * Removed unneccesary files
    
    * code updated
    
    * downlaod added
    
    * removed print
    
    * sort import
    
    * remove files added by nikhil
    
    * code renamed
    
    * flake8 happy
    
    * code improved
    
    * code improved
    
    * path updated
    
    * class added
    
    * train removed
    
    * removed resnet support
    
    * Update README.md
    
    * flake 8 happy
    
    * reame upated
    
    * scan on artifact level
    
    * updated inference
    
    * updated code
    
    * time updated
    
    * code updated
    
    Co-authored-by: gangooteli <24511086+gangooteli@users.noreply.github.com>

---
+++ cgmml/models/HRNET/1_download_model.py
@@ -0,0 +1,23 @@
+from azureml.core import Workspace
+from azureml.core.model import Model
+from models.HRNET.code.config import cfg, update_config
+
+
+def download_model(workspace, model_name, target_path):
+    '''
+    Download the pretrained model
+    Input:
+         workspace: workspace to access the experiment
+         model_name: Name of the model in which model is registered
+         target_path: Where model should download
+    '''
+    model = Model(workspace, name=model_name)
+    model.download(target_dir=target_path, exist_ok=True, exists_ok=None)
+
+
+if __name__ == '__main__':
+    workspace = Workspace.from_config()
+
+    args = 'src/models/HRNET/inference-config-hrnet.yaml'
+    update_config(cfg, args)
+    download_model(workspace=workspace, model_name=cfg.MODEL.NAME, target_path='src/models/HRNET')
